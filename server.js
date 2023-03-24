efa249fd69602f5c3044b61606093ecafeffc479
MD MONIRUZZAMAN <git config --global user.email>
2023-03-24 11:14:45 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const port = 5000
+const app = express()
+function getClientIp (req) {
+    const forwardedFor = req.headers['x-forwarded-for']
+    if (forwardedFor) {
+      const ips = forwardedFor.split(',')
+      return ips[0].trim()
+    } else {
+      return req.ip
+    }
+  }
+
+app.get('/', (req, res)=>{
+    const clientIp = getClientIp(req)
+  res.send(`Your IP address is: ${clientIp}`)
+})
+
+app.listen(port, () => {
+    console.log('Running Server Done', port)
+  })
+  
