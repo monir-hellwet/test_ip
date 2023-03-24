abc7d9d113367e2b0a1b6625333ae6b66555f99c
MD MONIRUZZAMAN <git config --global user.email>
2023-03-24 12:51:47 +0600

    update

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const port = 5000
+// imports
+import express from 'express'
+// import cors from 'cors'
+// import dotenv from 'dotenv'
+// import useRouter from './routes/userRoute.js'
+// import connectDBs from './config/dbConnect.js'
+// import requestIp from 'request-ip'
 const app = express()
-function getClientIp (req) {
-    const forwardedFor = req.headers['x-forwarded-for']
-    if (forwardedFor) {
-      const ips = forwardedFor.split(',')
-      return ips[0].trim()
-    } else {
-      return req.ip
-    }
-  }
+// dotenv.config()
+// // Middleware
+// app.use(cors())
+// app.use(express.json())
+// app.set('trust proxy', true)
+// const DATABASE_URL = process.env.SERVER_URl
+// const DATABASE_NAME = process.env.SERVER_NAME
+// connectDBs(DATABASE_URL, DATABASE_NAME)
 
-app.get('/', (req, res)=>{
-    res.send("run server")
-//     const clientIp = getClientIp(req)
-//   res.send(`Your IP address is: ${clientIp}`)
-})
-app.get('/check', (req, res)=>{
-    const clientIp = getClientIp(req)
-  res.send(`Your IP address is: ${clientIp}`)
+// Routes
+// app.use('/server', useRouter)
+app.get('/', (req, res) => {
+  res.send('server check')
 })
 
+// listen to port
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-    console.log('Running Server Done', port)
-  })
-  
+  console.log('Running Server Done', port)
+})
